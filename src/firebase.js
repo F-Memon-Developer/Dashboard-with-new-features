d6ec944f92107611ff71e61802b3d35bab198d0c
Memon <memon2572006f@gmail.com>
2025-08-15 15:57:19 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,16 +5,12 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "dashboard-11807.firebaseapp.com",
-  projectId: "dashboard-11807",
-  storageBucket: "dashboard-11807.firebasestorage.app",
-  messagingSenderId: "428832791856",
-  appId: "1:428832791856:web:5e65572598359ce26fbcf5"
+ "use your own id"
 };
 
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth();
+
 export const storage = getStorage(app);
